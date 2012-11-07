60897a0f5fd69c6eadfa119ab85fd7b5455b393f
singularity-sg <singularity.sg@gmail.com>
2012-11-07 23:33:00 +0800

    Added the basic features

--- app/controllers/Iteration.java
+++ app/controllers/Iteration.java
@@ -1,26 +1,43 @@
 package controllers;
 
-import java.util.List;
-
+import com.orientechnologies.orient.core.id.ORecordId;
+import models.Fibonacci;
+import models.Story;
 import play.mvc.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Iteration extends Controller {
-	
+
+    public enum MODE {CREATE,UPDATE,DELETE};
+
 	public static void add() {
-		String mode = "add";
+        MODE mode = MODE.CREATE;
 		renderTemplate("Iteration/form.html", mode);
 	}
 	
-	public static void edit(String id) {
-		
+	public static void edit(ORecordId id) {
+		models.Iteration iteration = models.Iteration.findById(id);
+        MODE mode = MODE.UPDATE;
+        renderTemplate("Iteration/form.html", mode, iteration);
 	}
 	
-	public static void delete(String id) {
-		
+	public static void delete(ORecordId id) {
+		models.Iteration iteration = models.Iteration.findById(id);
+        iteration.delete();
+
+        redirect("Application.index");
 	}
-	
-	public static void save() {
-	
+
+    public static void save(ORecordId id) {
+        models.Iteration iteration = models.Iteration.findById(id);
+
+        iteration.name = params.get("name");
+        iteration.description = params.get("description");
+        iteration.save();
+
+        redirect("Application.index");
 	}
 	
 	public static void create() {
@@ -28,11 +45,20 @@ public class Iteration extends Controller {
 		models.Iteration iteration = new models.Iteration();
 		iteration.name = params.get("name");
 		iteration.description = params.get("description");
+        iteration.stories = new ArrayList<Story>();
+
+        models.Story story = new models.Story();
+        story.name = "US1234";
+        story.description = "Test";
+        story.storyPoints = Fibonacci.EIGHT;
+        story.actualHours = 40;
+
+        iteration.stories.add(story);
+
 		iteration.save();
-		
-		List<models.Iteration> iterations = models.Iteration.find("select * from Iteration");
-		
-		renderTemplate("Application/index.html", iterations);
+
+        redirect("Application.index");
+
 	}
 
 }
---
+++ app/controllers/Story.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import com.orientechnologies.orient.core.id.ORecordId;
+import play.mvc.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: han
+ * Date: 11/7/12
+ * Time: 11:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Story extends Controller {
+
+    public static void create() {
+
+    }
+
+    public static void edit(ORecordId id) {
+
+    }
+
+    public static void delete(ORecordId id) {
+
+    }
+
+}
