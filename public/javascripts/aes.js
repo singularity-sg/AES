cbacfc8edc27b2b0ddf7cf82a3b8e9baad83b80e
SG Development Team <sgextreme@wds.co>
2012-12-05 09:20:38 +0800

    New commit with deletion confirmation

--- public/javascripts/aes.js
+++ public/javascripts/aes.js
@@ -11,3 +11,9 @@ function openDialog(title, message, color) {
 	$("#message-dialog").dialog({ closeOnEscape: true, title: title, modal: true, buttons: [ { text: "Ok", click: function() { $( this ).dialog( "close" ); }}]});
 	$("#message-dialog").dialog("open");
 }
+
+function openConfirmation(title, message, deletionLink) {
+	$("#message-dialog div.message").text(message);
+	$("#message-dialog").dialog({ closeOnEscape: true, title: title, modal: true, buttons: [ { text: "Ok", click: function() { document.location.href = deletionLink; $( this ).dialog( "close" );}} , { text: "Cancel", click: function() { $(this).dialog("close");}} ]});
+	$("#message-dialog").dialog("open");
+}
